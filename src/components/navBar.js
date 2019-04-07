007d7e8d0cbd8206b384ed8d3a30db742f8e6487
enoler <enolin7@gmail.com>
2019-04-07 17:44:26 +0200

    Added navbar

---
+++ src/components/navBar.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Navbar } from 'react-bootstrap';
+
+export default class NavBarComponent extends Component {
+  constructor (args) {
+    super(args);
+  }
+
+  onChange (event) {
+    const inputAnswer = parseInt(event.currentTarget.value);
+    console.log(inputAnswer);
+  }
+
+  render () {
+    return (
+      <Navbar bg='dark' variant='dark'>
+        <Navbar.Brand href='#home'>
+          <img
+            alt=''
+            src='../assets/logo.svg'
+            width='30'
+            height='30'
+            className='d-inline-block align-top' />
+          {'React Quiz'}
+        </Navbar.Brand>
+        <Navbar.Collapse className='justify-content-end'>
+          <Navbar.Text>
+            Round: <a href='#login'>0</a>
+          </Navbar.Text>
+        </Navbar.Collapse>
+      </Navbar>
+    );
+  }
+}
